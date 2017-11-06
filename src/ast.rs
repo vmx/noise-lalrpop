f0c9a9e336f0e5220d303f5377f9c17c18df3899
Volker Mische <volker.mische@gmail.com>
2017-11-06 17:06:41 +0100

    Start support for booleans

--- src/ast.rs
+++ src/ast.rs
@@ -3,9 +3,17 @@ pub enum OperatorType {
     Equal,
 }
 
+#[derive(Debug)]
+pub enum BoolType {
+    And,
+}
+
 #[derive(Debug, PartialEq)]
 pub enum Token {
     Literal(String),
     Object(String, Box<Token>),
+
     Equal(String, String),
+
+    And(Box<Token>, Box<Token>),
 }
--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,11 @@ fn noise() {
                r#"Ok(Object("hello", Equal("nested", "world")))"#);
     assert_eq!(format!("{:?}", noise::parse_Noise(r#"find {"hello": {"deeply": {"nested": == "world"}}}"#)),
                r#"Ok(Object("hello", Object("deeply", Equal("nested", "world"))))"#);
+    assert_eq!(format!("{:?}", noise::parse_Noise(r#"find {"hello": == "world", "another": == "one"}"#)),
+               r#"Ok(And(Equal("hello", "world"), Equal("another", "one")))"#);
+    assert_eq!(format!("{:?}", noise::parse_Noise(r#"find {"hello": == "world", "another": == "one", "third": == "thing"}"#)),
+               r#"Ok(And(And(Equal("hello", "world"), Equal("another", "one")), Equal("third", "thing")))"#);
 
-    let out = noise::parse_Noise(r#"find {"hello": {"nested": == "world"}}"#);
+    let out = noise::parse_Noise(r#"find {"hello": == "world", "another": == "one", "third": == "thing"}"#);
     println!("out: {:?}", out);
 }
