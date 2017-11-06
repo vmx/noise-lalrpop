fc77c7d079eeaffc3193037fba290d79836d347a
Volker Mische <volker.mische@gmail.com>
2017-11-06 15:59:03 +0100

    Make object nesting possible

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,8 @@ fn noise() {
                r#"Ok(Equal("hello", "world"))"#);
     assert_eq!(format!("{:?}", noise::parse_Noise(r#"find {"hello": {"nested": == "world"}}"#)),
                r#"Ok(Object("hello", Equal("nested", "world")))"#);
+    assert_eq!(format!("{:?}", noise::parse_Noise(r#"find {"hello": {"deeply": {"nested": == "world"}}}"#)),
+               r#"Ok(Object("hello", Object("deeply", Equal("nested", "world"))))"#);
 
     let out = noise::parse_Noise(r#"find {"hello": {"nested": == "world"}}"#);
     println!("out: {:?}", out);
