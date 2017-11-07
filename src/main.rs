3d29fb750883b464c461102dc460549b73981ad8
Volker Mische <volker.mische@gmail.com>
2017-11-07 11:55:24 +0100

    Add some comments

--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,19 @@ pub mod ast;
 
 #[test]
 fn noise() {
+    // Simple
     assert_eq!(format!("{:?}", noise::parse_Noise(r#"find {"hello": == "world"}"#)),
                r#"Ok(Equal("hello", "world"))"#);
+
+    // Nested
     assert_eq!(format!("{:?}", noise::parse_Noise(r#"find {"hello": {"nested": == "world"}}"#)),
                r#"Ok(Object("hello", Equal("nested", "world")))"#);
     assert_eq!(format!("{:?}", noise::parse_Noise(r#"find {"hello": {"deeply": {"nested": == "world"}}}"#)),
                r#"Ok(Object("hello", Object("deeply", Equal("nested", "world"))))"#);
     assert_eq!(format!("{:?}", noise::parse_Noise(r#"find {"hello": == "world", "another": == "one"}"#)),
                r#"Ok(And(Equal("hello", "world"), Equal("another", "one")))"#);
+
+    // Boolean
     assert_eq!(format!("{:?}", noise::parse_Noise(r#"find {"hello": == "world", "another": == "one", "third": == "thing"}"#)),
                r#"Ok(And(And(Equal("hello", "world"), Equal("another", "one")), Equal("third", "thing")))"#);
     assert_eq!(format!("{:?}", noise::parse_Noise(r#"find {"hello": == "world", "another": == "one" || "third": == "thing"}"#)),
