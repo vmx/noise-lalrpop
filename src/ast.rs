24c244d1b6dadf383973430ba74ab4b2ec1b9dee
Volker Mische <volker.mische@gmail.com>
2017-11-07 17:27:05 +0100

    Support arrays

--- src/ast.rs
+++ src/ast.rs
@@ -11,11 +11,13 @@ pub enum BoolType {
 
 #[derive(Debug, PartialEq)]
 pub enum Token {
+    Array(Box<Token>),
     Literal(String),
     Object(String, Box<Token>),
     Boost(u64, Box<Token>),
 
-    Equal(String, String),
+    // If the key is `None` then it's an operator within an array
+    Equal(Option<String>, String),
 
     And(Box<Token>, Box<Token>),
     Or(Box<Token>, Box<Token>),
--- src/main.rs
+++ src/main.rs
@@ -5,46 +5,54 @@ pub mod ast;
 fn noise() {
     // Simple
     assert_eq!(format!("{:?}", noise::parse_Noise(r#"find {"hello": == "world"}"#)),
-               r#"Ok(Equal("hello", "world"))"#);
+               r#"Ok(Equal(Some("hello"), "world"))"#);
 
     // Nested
     assert_eq!(format!("{:?}", noise::parse_Noise(r#"find {"hello": {"nested": == "world"}}"#)),
-               r#"Ok(Object("hello", Equal("nested", "world")))"#);
+               r#"Ok(Object("hello", Equal(Some("nested"), "world")))"#);
     assert_eq!(format!("{:?}", noise::parse_Noise(r#"find {"hello": {"deeply": {"nested": == "world"}}}"#)),
-               r#"Ok(Object("hello", Object("deeply", Equal("nested", "world"))))"#);
+               r#"Ok(Object("hello", Object("deeply", Equal(Some("nested"), "world"))))"#);
     assert_eq!(format!("{:?}", noise::parse_Noise(r#"find {"hello": == "world", "another": == "one"}"#)),
-               r#"Ok(And(Equal("hello", "world"), Equal("another", "one")))"#);
+               r#"Ok(And(Equal(Some("hello"), "world"), Equal(Some("another"), "one")))"#);
 
     // Boolean
     assert_eq!(format!("{:?}", noise::parse_Noise(r#"find {"hello": == "world", "another": == "one", "third": == "thing"}"#)),
-               r#"Ok(And(And(Equal("hello", "world"), Equal("another", "one")), Equal("third", "thing")))"#);
+               r#"Ok(And(And(Equal(Some("hello"), "world"), Equal(Some("another"), "one")), Equal(Some("third"), "thing")))"#);
     assert_eq!(format!("{:?}", noise::parse_Noise(r#"find {"hello": == "world", "another": == "one" || "third": == "thing"}"#)),
-               r#"Ok(Or(And(Equal("hello", "world"), Equal("another", "one")), Equal("third", "thing")))"#);
+               r#"Ok(Or(And(Equal(Some("hello"), "world"), Equal(Some("another"), "one")), Equal(Some("third"), "thing")))"#);
     assert_eq!(format!("{:?}", noise::parse_Noise(r#"find {"hello": == "world" || "another": == "one", "third": == "thing"}"#)),
-               r#"Ok(Or(Equal("hello", "world"), And(Equal("another", "one"), Equal("third", "thing"))))"#);
+               r#"Ok(Or(Equal(Some("hello"), "world"), And(Equal(Some("another"), "one"), Equal(Some("third"), "thing"))))"#);
     assert_eq!(format!("{:?}", noise::parse_Noise(r#"find {"hello": == "world" || "another": == "one" || "third": == "thing"}"#)),
-               r#"Ok(Or(Or(Equal("hello", "world"), Equal("another", "one")), Equal("third", "thing")))"#);
+               r#"Ok(Or(Or(Equal(Some("hello"), "world"), Equal(Some("another"), "one")), Equal(Some("third"), "thing")))"#);
     assert_eq!(format!("{:?}", noise::parse_Noise(r#"find {"hello": == "world" && "another": == "one" || "third": == "thing"}"#)),
-               r#"Ok(Or(And(Equal("hello", "world"), Equal("another", "one")), Equal("third", "thing")))"#);
+               r#"Ok(Or(And(Equal(Some("hello"), "world"), Equal(Some("another"), "one")), Equal(Some("third"), "thing")))"#);
 
     // Parenthesis
     assert_eq!(format!("{:?}", noise::parse_Noise(r#"find {"hello": == "world", ("another": == "one" || "third": == "thing")}"#)),
-               r#"Ok(And(Equal("hello", "world"), Or(Equal("another", "one"), Equal("third", "thing"))))"#);
+               r#"Ok(And(Equal(Some("hello"), "world"), Or(Equal(Some("another"), "one"), Equal(Some("third"), "thing"))))"#);
     assert_eq!(format!("{:?}", noise::parse_Noise(r#"find {"hello": == "world", ("another": == "one", "third": == "thing")}"#)),
-               r#"Ok(And(Equal("hello", "world"), And(Equal("another", "one"), Equal("third", "thing"))))"#);
+               r#"Ok(And(Equal(Some("hello"), "world"), And(Equal(Some("another"), "one"), Equal(Some("third"), "thing"))))"#);
+
+    // Arrays
+    assert_eq!(format!("{:?}", noise::parse_Noise(r#"find {"hello": [{"array": == "world"}]}"#)),
+               r#"Ok(Object("hello", Array(Equal(Some("array"), "world"))))"#);
+    assert_eq!(format!("{:?}", noise::parse_Noise(r#"find {"hello": [{"array": == "world", "another": == "one"}]}"#)),
+               r#"Ok(Object("hello", Array(And(Equal(Some("array"), "world"), Equal(Some("another"), "one")))))"#);
+    assert_eq!(format!("{:?}", noise::parse_Noise(r#"find {"hello": [== "world"]}"#)),
+               r#"Ok(Object("hello", Array(Equal(None, "world"))))"#);
 
     // Boost
     assert_eq!(format!("{:?}", noise::parse_Noise(r#"find {"hello": == "world"^2}"#)),
-               r#"Ok(Boost(2, Equal("hello", "world")))"#);
+               r#"Ok(Boost(2, Equal(Some("hello"), "world")))"#);
     assert_eq!(format!("{:?}", noise::parse_Noise(r#"find {"hello": == "world"^2, "another": == "one"}"#)),
-               r#"Ok(And(Boost(2, Equal("hello", "world")), Equal("another", "one")))"#);
+               r#"Ok(And(Boost(2, Equal(Some("hello"), "world")), Equal(Some("another"), "one")))"#);
     assert_eq!(format!("{:?}", noise::parse_Noise(r#"find {("hello": == "world")^2}"#)),
-               r#"Ok(Boost(2, Equal("hello", "world")))"#);
+               r#"Ok(Boost(2, Equal(Some("hello"), "world")))"#);
     assert_eq!(format!("{:?}", noise::parse_Noise(r#"find {"hello": == "world"}^2"#)),
-               r#"Ok(Boost(2, Equal("hello", "world")))"#);
+               r#"Ok(Boost(2, Equal(Some("hello"), "world")))"#);
     assert_eq!(format!("{:?}", noise::parse_Noise(r#"find {"hello": {"nested": == "world"}^2}"#)),
-               r#"Ok(Object("hello", Boost(2, Equal("nested", "world"))))"#);
+               r#"Ok(Object("hello", Boost(2, Equal(Some("nested"), "world"))))"#);
 
-    let out = noise::parse_Noise(r#"find {"hello": == "world"}^2"#);
+    let out = noise::parse_Noise(r#"find {"hello": [== "world"]}"#);
     println!("out: {:?}", out);
 }
